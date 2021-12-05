5b4bc5d779f7f808b401fbe22d525edb92d35baa
seeenlee <naeseel18@gmail.com>
2021-12-04 23:02:55 -0500

    setup Cell

--- src/BackEnd/Cell.java
+++ src/BackEnd/Cell.java
@@ -1,4 +1,13 @@
 package BackEnd;
 
+import java.awt.*;
+
 public class Cell {
+    int[] position;
+    Color color;
+
+    public Cell(int[] position, Color color) {
+        this.position = position;
+        this.color = color;
+    }
 }
