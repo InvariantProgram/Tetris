3f4700e120c6e78359ad39c980aa8cb650a3f7f6
Kevin Zhang <71031989+InvariantProgram@users.noreply.github.com>
2021-12-04 23:32:48 -0500

    Update Board.java

--- src/BackEnd/Board.java
+++ src/BackEnd/Board.java
@@ -1,4 +1,22 @@
 package BackEnd;
 
 public class Board {
+    private static final int columns = 10;
+    private static final int maxHeight = 20;
+
+    private Cell[][] tetrisBoard;
+    private int[] colMax;
+
+    public Board() {
+        tetrisBoard = new Cell[maxHeight][columns];
+        colMax = new int[columns];
+    }
+
+    public Cell[][] getTetrisBoard() {
+        return tetrisBoard;
+    }
+
+    public int getMaxInCol(int column) {
+        return colMax[column];
+    }
 }
