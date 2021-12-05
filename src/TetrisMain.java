752d6247e797c0f1c9c85920d759679d40991499
seeenlee <naeseel18@gmail.com>
2021-12-04 23:57:37 -0500

    started TetrisMain

--- src/TetrisMain.java
+++ src/TetrisMain.java
@@ -1,2 +1,53 @@
-public class TetrisMain {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class TetrisMain extends JComponent implements Runnable {
+    private TetrisMain tetrisMain;
+
+    public TetrisMain() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                System.out.println(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                System.out.println(e);
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e.getPoint());
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println(e.getPoint());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new TetrisMain());
+    }
+    public void run() {
+        JFrame frame = new JFrame("Tetris");
+        tetrisMain = new TetrisMain();
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    protected void paintComponent(Graphics g) {
+    }
+
 }
